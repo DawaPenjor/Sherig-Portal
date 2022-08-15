151130d36e5acc94de414152115676f268099b08
Dawa Penjor <63368334+DawaPenjor@users.noreply.github.com>
2022-08-15 16:46:15 +0600

    Update

--- TrashigangSherigInfo/settings.py
+++ TrashigangSherigInfo/settings.py
@@ -162,5 +162,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thewapenjor@education.gov.bt'
-EMAIL_HOST_PASSWORD = 'Dawa@1994'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
